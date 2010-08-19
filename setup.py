2ba10784752904a7042d8c495bacd0df1e71d1a3
shaunsephton <shaunsephton@gmail.com>
2010-08-19 11:53:49 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='database-routers',
+    version='0.0.1',
+    description="Collection of handy Django database routers for use with Django's multiple database API.",
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/django-database-routers',
+    packages = find_packages(),
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
