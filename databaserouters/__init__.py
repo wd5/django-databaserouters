27461510f78db1cfa75fafa9c570b97a52969183
shaunsephton <shaunsephton@gmail.com>
2010-08-19 14:47:09 +0200

    first commit

--- databaserouters/__init__.py
+++ databaserouters/__init__.py
@@ -1,48 +1,71 @@
+from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 class AppModelRouter(object):
     """
-    A router to control all database operations based on APP_MODEL_DATABASE_ROUTING settings
+    A router to control all database operations based on APP_MODEL_DATABASE_ROUTING settings.
     """
+    def __init__(self, *args, **kwargs):
+        self.check_configuration()
+
+    @property
+    def settings(self):
+        return settings.APP_MODEL_DATABASE_ROUTING
+
     def check_configuration(self):
         """
-        Check that all things needed to run this router have been correctly configured
+        Check that all things needed to run this router have been correctly configured.
         """
-        if not settings.APP_MODEL_DATABASE_ROUTING:
-            raise ImproperlyConfigured("AppModelRouter requires am APP_MODEL_DATABASE_ROUTING setting.")
-
+        #If no APP_MODEL_DATABASE_ROUTING settings is specified an ImproperlyConfigured
+        #exception should be raised.
+        try:
+            settings.APP_MODEL_DATABASE_ROUTING
+        except AttributeError:
+            raise ImproperlyConfigured("AppModelRouter requires an APP_MODEL_DATABASE_ROUTING setting.")
+        
+        #If an invalid APP_MODEL_DATABASE_ROUTING setting is specified an ImproperlConfigured
+        #exception should be raised.
+        if settings.APP_MODEL_DATABASE_ROUTING.__class__ != dict:
+            raise ImproperlyConfigured("AppModelRouter requires a dictionary for APP_MODEL_DATABASE_ROUTING setting.")
+            
     def db_for_read(self, model, **hints):
         """
-        XXX
+        Read the model from the database specified for it under the db_for_read router. 
         """
-        if model._meta.app_label == 'myapp':
-            return 'other'
+        dbs = self.resolve_dbs_for_router_by_model(router='db_for_read', model=model)
+        if dbs:
+            return dbs[0]
         return None
 
     def db_for_write(self, model, **hints):
         """
-        XXX
+        Write the model to the database specified for it under the db_for_write router. 
         """
-        "Point all operations on myapp models to 'other'"
-        if model._meta.app_label == 'myapp':
-            return 'other'
-        return None
-
-    def allow_relation(self, obj1, obj2, **hints):
-        """
-        XXX
-        """
-        if obj1._meta.app_label == 'myapp' or obj2._meta.app_label == 'myapp':
-            return True
+        dbs = self.resolve_dbs_for_router_by_model(router='db_for_write', model=model)
+        if dbs:
+            return dbs[0]
         return None
 
     def allow_syncdb(self, db, model):
         """
-        XXX
+        syncdb for all models specified for the db under the allow_syncdb router. 
         """
-        if db == 'other':
-            return model._meta.app_label == 'myapp'
-        elif model._meta.app_label == 'myapp':
-            return False
-        return None
+        dbs = self.resolve_dbs_for_router_by_model(router='allow_syncdb', model=model)
+        return (db in dbs)
+        
+    def resolve_dbs_for_router_by_model(self, model, router):
+        """
+        Determines which dbs have been configured for the given router and model.
+        """
+        dbs = []
+        app_model_name = "%s.%s" % (model._meta.app_label, model._meta.object_name.lower())
+        try:
+            router = self.settings[router]
+        except KeyError:
+            return dbs
+           
+        for key, values in router.items():
+            if app_model_name in values:
+                dbs.append(key)
 
+        return dbs
--- databaserouters/tests.py
+++ databaserouters/tests.py
@@ -1,20 +1,114 @@
 import unittest
 
+from django.db import models
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+
+class TestModel(models.Model):
+    pass
+models.register_models('databaserouters', TestModel)
 
 class AppModelRouterTestCase(unittest.TestCase):
-    from databaserouter import AppModelRouter
+
+    def setUp(self):
+        from databaserouters import AppModelRouter
+        settings.APP_MODEL_DATABASE_ROUTING = {}
+        self.router = AppModelRouter()
 
     def test_check_configuration(self):
-        
-        
-        raise NotImplementedError
+        # If no APP_MODEL_DATABASE_ROUTING settings is specified an ImproperlyConfigured
+        # exception should be raised.
+        try:
+            del settings.APP_MODEL_DATABASE_ROUTING
+        except AttributeError:
+            pass
+        self.assertRaises(ImproperlyConfigured, self.router.check_configuration)     
+
+        # If an invalid APP_MODEL_DATABASE_ROUTING setting is specified an ImproperlConfigured
+        # exception should be raised.
+        settings.APP_MODEL_DATABASE_ROUTING = None
+        self.assertRaises(ImproperlyConfigured, self.router.check_configuration)     
         
     def test_allow_syncdb(self):
-        raise NotImplementedError
+        # Return False if the model is not specified for the allow_syncdb router.
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'allow_syncdb': {'default': []},
+        }
+        self.failIf(self.router.allow_syncdb(db='default', model=TestModel))
+        
+        # Return False if the model is specified for the allow_syncdb router but not for the database.
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'allow_syncdb': {'invalid_database': ['databaserouters.testmodel']},
+        }
+        self.failIf(self.router.allow_syncdb(db='default', model=TestModel))
+        
+        # Return True if the model is specified for the allow_syncdb router and database.
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'allow_syncdb': {'default': ['databaserouters.testmodel']},
+        }
+        self.failUnless(self.router.allow_syncdb(db='default', model=TestModel))
     
     def test_db_for_read(self):
-        raise NotImplementedError
+        # Return None if the the model is not specified for the db_for_read router
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'db_for_read': {
+                'default': [],
+            },
+        }
+        self.failIf(self.router.db_for_read(model=TestModel))
         
+        # Return the first database the the model is specified for under the db_for_read router
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'db_for_read': {
+                'default': ['databaserouters.testmodel'],
+                'some_other_database': ['databaserouters.testmodel'],
+            },
+        }
+        self.failUnless(self.router.db_for_read(model=TestModel) == 'default')
+    
     def test_db_for_write(self):
-        raise NotImplementedError
+        # Return None if the the model is not specified for the db_for_write router
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'db_for_write': {
+                'default': [],
+            },
+        }
+        self.failIf(self.router.db_for_write(model=TestModel))
+        
+        # Return the first database the the model is specified for under the db_for_write router
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'db_for_write': {
+                'default': ['databaserouters.testmodel'],
+                'some_other_database': ['databaserouters.testmodel'],
+            },
+        }
+        self.failUnless(self.router.db_for_write(model=TestModel) == 'default')
+   
+    def test_resolve_dbs_for_router_by_model(self):
+        # Return the empty list if an invalid router is specified.
+        self.failUnless(self.router.resolve_dbs_for_router_by_model(model=TestModel, router='invalid_router') == [])
+        
+        # Return the empty list if an valid router is specified but without any databases.
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'allow_syncdb': {}
+        }
+        self.failUnless(self.router.resolve_dbs_for_router_by_model(model=TestModel, router='allow_syncdb') == [])
+        
+        # Return the empty list if an valid router is specified with a database but without any models.
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'allow_syncdb': {'default': []}
+        }
+        self.failUnless(self.router.resolve_dbs_for_router_by_model(model=TestModel, router='allow_syncdb') == [])
+        
+        # Return the empty list if an valid router is specified with a database but with an invalid model.
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'allow_syncdb': {'default': ['invalid.model']}
+        }
+        self.failUnless(self.router.resolve_dbs_for_router_by_model(model=TestModel, router='allow_syncdb') == [])
+        
+        # Return the list of databases if a valid router is specified with a database and models.
+        settings.APP_MODEL_DATABASE_ROUTING = {
+            'allow_syncdb': {'default': ['databaserouters.testmodel']}
+        }
+        self.failUnless('default' in self.router.resolve_dbs_for_router_by_model(model=TestModel, router='allow_syncdb'))
